00f1e19406970c5b7eea83c1a038ed869db90091
Brian Brown <brian@thekompanee.com>
2016-08-03 15:26:46 -0500

    Enqueue fonts, JavaScript and CSS

--- functions.php
+++ functions.php
@@ -148,11 +148,25 @@ add_action( 'widgets_init', 'water_press_widgets_init' );
  * Enqueue scripts and styles.
  */
 function water_press_scripts() {
-	wp_enqueue_style( 'water-press-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'water-press-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    $theme_name = wp_get_theme();
+    $version = $theme_name['Version'];
 
-	wp_enqueue_script( 'water-press-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    $font_query_args = array(
+		'family' => 'Roboto+Condensed:400,700|Roboto:400,700,300,900',
+	);
+
+    wp_enqueue_style( 'water-press-google-fonts', add_query_arg( $font_query_args, "//fonts.googleapis.com/css" ) );
+    wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/fonts/genericons/genericons/genericons.css' );
+    wp_enqueue_style( 'water-press-style', get_stylesheet_uri(), '', $version );
+
+    wp_enqueue_script( 'masonry' );
+    wp_enqueue_script( 'scrollnav', get_template_directory_uri() . '/js/scroll-nav.js', array('jquery'), '3.0.0', true );
+    wp_enqueue_script( 'water-press-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+    wp_register_script( 'water-press-custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), $version, true );
+    wp_localize_script( 'water-press-custom', 'water_press_data', $array );
+    wp_enqueue_script( 'water-press-custom' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
