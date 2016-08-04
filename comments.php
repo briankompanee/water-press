7e8480572d3b09173319db44d17d293a130cd0c1
Brian Brown <brian@thekompanee.com>
2016-08-04 00:42:15 -0500

    style comments

--- comments.php
+++ comments.php
@@ -28,30 +28,19 @@ if ( post_password_required() ) {
 		<h2 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'water-press' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+					esc_html( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'water-press' ) ),
+					number_format_i18n( get_comments_number() )
 				);
 			?>
 		</h2>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'water-press' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'water-press' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'water-press' ) ); ?></div>
-
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+                    'callback'   => 'water_press_theme_comment',
+                    'avatar_size'=> 91,
 				) );
 			?>
 		</ol><!-- .comment-list -->
@@ -79,7 +68,57 @@ if ( post_password_required() ) {
 	<?php
 	endif;
 
-	comment_form();
+
+    //https://codex.wordpress.org/Function_Reference/comment_form
+    $commenter = wp_get_current_commenter();
+    $req = get_option( 'require_name_email' );
+    $aria_req = ( $req ? " aria-required='true'" : '' );
+
+    $fields =  array(
+        'author' =>
+            '<p class="comment-form-author"><label for="author">' . __( 'Name', 'water-press' ) . '<span class="required">*</span></label><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+            '" size="30"' . $aria_req . ' /></p>',
+        'email' =>
+            '<p class="comment-form-email"><label for="email">' . __( 'Email', 'water-press' ) . '<span class="required">*</span></label><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+            '" size="30"' . $aria_req . ' /></p>',
+        'url' =>
+            '<p class="comment-form-url"><label for="url">' . __( 'Website', 'water-press' ) . '</label><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+            '" size="30" /></p>',
+    );
+
+    $comment_arg = array(
+        'id_form'           => 'commentform',
+        'id_submit'         => 'submit',
+        'class_submit'      => 'submit',
+        'name_submit'       => 'submit',
+        'title_reply'       => __( 'Leave a Reply', 'water-press' ),
+        'title_reply_to'    => __( 'Leave a Reply to %s', 'water-press' ),
+        'cancel_reply_link' => __( 'Cancel Reply', 'water-press' ),
+        'label_submit'      => __( 'POST COMMENT', 'water-press' ),
+        'format'            => 'xhtml',
+
+        'comment_field' =>  '<p class="comment-form-comment"><label for="comment">' . __( 'Comment', 'water-press' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true">' .
+            '</textarea></p>',
+
+        'must_log_in' => '<p class="must-log-in">' .
+        sprintf(
+        __( 'You must be <a href="%s">logged in</a> to post a comment.', 'water-press' ),
+        wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+        ) . '</p>',
+
+        'logged_in_as' => '<p class="logged-in-as">' .
+        sprintf(
+        __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>', 'water-press' ),
+        admin_url( 'profile.php' ),
+        $user_identity,
+        wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+        ) . '</p>',
+
+        'comment_notes_after' => '',
+
+        'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+    );
+	comment_form( $comment_arg );
 	?>
 
 </div><!-- #comments -->
