a17171e674b7cadf357fec01a048b745b69c791d
Brian Brown <brian@thekompanee.com>
2016-08-04 16:09:12 -0500

    Add social support and footer content

--- inc/extras.php
+++ inc/extras.php
@@ -119,7 +119,7 @@ function water_press_theme_comment( $comment, $args, $depth ){
  * Function to get Sections
  */
 function water_press_get_sections(){
-    $sections = array( 'about', 'cta1', 'blog', 'experience', 'portfolio', 'cta2', 'contact' );
+    $sections = array( 'hero', 'about', 'cta1', 'blog', 'experience', 'portfolio', 'cta2', 'contact' );
     $enabled_section = array();
     foreach ( $sections as $section ){
         if ( esc_attr( get_theme_mod( 'water_press_ed_' . $section . '_section' ) ) == 1 ){
@@ -130,4 +130,92 @@ function water_press_get_sections(){
         }
     }
     return $enabled_section;
+}
+/**
+ * Callback for Social Links
+*/
+function water_press_social_cb(){
+    $facebook = get_theme_mod( 'water_press_facebook' );
+    $twitter = get_theme_mod( 'water_press_twitter' );
+    $pinterest = get_theme_mod( 'water_press_pinterest' );
+    $linkedin = get_theme_mod( 'water_press_linkedin' );
+    $google_plus = get_theme_mod( 'water_press_google_plus' );
+
+    if( $facebook || $twitter || $pinterest || $linkedin || $google_plus ){ ?>
+    <ul class="social-networks">
+		<?php
+            if( $facebook ) echo '<li><a href="'. esc_url( $facebook ) .'" target="_blank" title="'. esc_html__( 'Facebook', 'water-press' ) .'"><span class="fa fa-facebook"></span></a></li>';
+            if( $twitter ) echo '<li><a href="'. esc_url( $twitter ) .'" target="_blank" title="'. esc_html__( 'Twitter', 'water-press' ) .'"><span class="fa fa-twitter"></span></a></li>';
+            if( $pinterest ) echo '<li><a href="'. esc_url( $pinterest ) .'" target="_blank" title="'. esc_html__( 'Pinterest', 'water-press' ) .'"><span class="fa fa-pinterest-p"></span></a></li>';
+            if( $linkedin ) echo '<li><a href="'. esc_url( $linkedin ) .'" target="_blank" title="'. esc_html__( 'LinkedIn', 'water-press' ) .'"><span class="fa fa-linkedin"></span></a></li>';
+            if( $google_plus ) echo '<li><a href="'. esc_url( $google_plus ) .'" target="_blank" title="'. esc_html__( 'Google Plus', 'water-press' ) .'"><span class="fa fa-google-plus"></a></li>';
+        ?>
+	</ul>
+    <?php }
+}
+add_action( 'water_press_social', 'water_press_social_cb' );
+
+/**
+ * Custom CSS
+*/
+function water_press_custom_css(){
+    $custom_css = get_theme_mod( 'water_press_custom_css' );
+    if( !empty( $custom_css ) ){
+		echo '<style type="text/css">';
+		echo wp_strip_all_tags( $custom_css );
+		echo '</style>';
+	}
+}
+add_action( 'wp_head', 'water_press_custom_css', 100 );
+
+if ( ! function_exists( 'water_press_excerpt_more' ) && ! is_admin() ) :
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with ... *
+ */
+function water_press_excerpt_more() {
+	return ' &hellip; ';
+}
+add_filter( 'excerpt_more', 'water_press_excerpt_more' );
+endif;
+
+if ( ! function_exists( 'water_press_excerpt_length' ) ) :
+/**
+ * Changes the default 55 character in excerpt
+*/
+function water_press_excerpt_length( $length ) {
+	return 40;
+}
+add_filter( 'excerpt_length', 'water_press_excerpt_length' );
+endif;
+
+/**
+ * excerpt length for portfolio section
+*/
+function water_press_excerpt_length_alt( $length ){
+    return 12;
+}
+
+/**
+ * Footer Credits
+*/
+function water_press_footer_credit(){
+    $text  = '<div class="site-info">';
+    $text .= sprintf( esc_html__( 'Copyright &copy;  %s', 'water-press' ), date_i18n( 'Y' ) . ' <a href="' . esc_url( home_url( '/' ) ) .'">' . esc_html( get_bloginfo( 'name' ) ) . '</a> &middot; ' );
+    $text .= sprintf( esc_html__( 'Powered by: %s', 'water-press' ), '<a href="'. esc_url( __( 'https://wordpress.org/', 'water-press' ) ) .'" target="_blank">WordPress</a>' );
+    $text .= '</div>';
+    echo apply_filters( 'water_press_footer_text', $text );
+}
+add_action( 'water_press_footer', 'water_press_footer_credit' );
+
+/**
+ * Return sidebar layouts for pages
+*/
+function water_press_sidebar_layout(){
+    global $post;
+
+    if( get_post_meta( $post->ID, 'water_press_sidebar_layout', true ) ){
+        return get_post_meta( $post->ID, 'water_press_sidebar_layout', true );
+    }else{
+        return 'right-sidebar';
+    }
 }
