e4c61ba10e97d73ba459c8b9d9ecf8d35b3a5020
Brian Brown <brian@thekompanee.com>
2016-08-04 00:26:38 -0500

    removal of required custom header file

--- functions.php
+++ functions.php
@@ -174,11 +174,6 @@ function water_press_scripts() {
 add_action( 'wp_enqueue_scripts', 'water_press_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
